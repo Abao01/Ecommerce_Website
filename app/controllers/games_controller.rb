8d4f6991f8f6c7805de2ffc69b4e7bed850cbea4
Abao01 <2519663066h@gmail.com>
2022-03-31 19:22:39 -0500

    Installed active storage

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,9 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.includes(:publisher).all
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ApplicationRecord
   belongs_to :publisher
+  validates :name, :price, :description, :role, presence: true
 
   validates :name, uniqueness: true
   validates :price, presence: true
+
+
 end
