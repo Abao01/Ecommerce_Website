3571c1333e3588187c2d8fca552a441554fe2e25
Abao01 <2519663066h@gmail.com>
2022-04-13 21:48:32 -0500

    Moditied application_controller file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+
+  private
+
+  def initialize_session
+    #this will initialize the visit count to zero for new users
+    session[:shopping_cart] ||= 0 #empty array that will products IDs
+  end
+
+  def cart
+    #collection of products available in our shopping cart
+    Game.find(session[:shopping_cart])
+  end
 
 end
